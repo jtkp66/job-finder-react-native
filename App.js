9c7b3fd02effbcb62102d8755d44cd203aae8c4a
Jamie Pham <jtkp66@gmail.com>
2018-10-03 20:41:20 -0700

    added facebook auth and navigation when logged in

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator, createStackNavigator } from 'react-navigation';
+import { Provider } from 'react-redux';
 
+import store from './store';
 import AuthScreen from './screens/AuthScreen';
 import WelcomeScreen from './screens/WelcomeScreen';
 import MapScreen from './screens/MapScreen';
@@ -26,12 +28,19 @@ export default class App extends React.Component {
           }
         })
       }
+    }, {
+      navigationOptions: {
+        tabBarVisible: false,
+      },
+      lazy: true
     });
 
     return (
+      <Provider store={store}>
       <View style={styles.container}>
         <MainNavigator />
       </View>
+      </Provider>
     );
   }
 }
