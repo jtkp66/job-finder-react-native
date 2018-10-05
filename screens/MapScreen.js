3d687bc07c2dcca62ff8574c088a0d30fdeebf8f
Jamie Pham <jtkp66@gmail.com>
2018-10-04 18:02:34 -0700

    added icons to tabBar for navigation, and android specific style changes

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
 import { connect } from 'react-redux';
-import { Button } from 'react-native-elements';
+import { Button, Icon } from 'react-native-elements';
 
 import * as actions from '../actions';
 
-// const API_KEY = 'AIzaSyBnJtKAn7O0bzwJcJ80lEXEZ-daqzuOM-Y';
-
 class MapScreen extends Component {
+    static navigationOptions = {
+        title: 'Map',      
+        tabBaricon: ({ tintColor }) => {
+            return <Icon name="my-location" size={30} color={tintColor}/>;
+        }
+        
+    }
+
     state = {
         mapLoaded: false,
         region: {
@@ -18,7 +24,6 @@ class MapScreen extends Component {
             latitudeDelta: 0.09
         }
     }
-    
 
     componentDidMount() {
         this.setState({ mapLoaded: true });
